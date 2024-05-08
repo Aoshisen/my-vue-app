0792456a2f7dd56f31b18dbd3ac71151a1d5b101
aoshisen <aoshisen.23@gmail.com>
2024-05-09 07:11:09 +0800

    feat: 互斥表单

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,8 +1,42 @@
 <script setup lang="ts">
-import { toRefs } from "vue";
+import { reactive, toRefs, watch } from "vue";
 import { useRoute } from "vue-router";
 const router = useRoute();
 const { params } = toRefs(router);
+const activeList = reactive([0, 0, 0]);
+const variableTypes = reactive([
+  {
+    name: "时间",
+    value: 1,
+    active: false,
+  },
+  {
+    name: "日订单均值",
+    value: 2,
+    active: false,
+  },
+  {
+    name: "插件金额占比",
+    value: 3,
+    active: false,
+  },
+]);
+watch(activeList, () => {
+  variableTypes.forEach((type) => {
+    type.active = activeList.includes(type.value);
+  });
+});
 </script>
 
-<template>this is about {{ params.id }}</template>
+<template>
+  this is about {{ params.id }}
+  <select v-for="item in 3" v-model="activeList[item - 1]">
+    <option
+      v-for="option in variableTypes"
+      :value="option.value"
+      :disabled="option.active"
+    >
+      {{ option.name }}
+    </option>
+  </select>
+</template>
